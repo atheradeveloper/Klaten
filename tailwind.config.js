108b4160ad5289823223f4460421bdbed9a0d098
Athera Developer <atheradeveloper@gmail.com>
2026-08-03 14:10:04 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,11 @@ module.exports = {
     extend: {
       colors: {
         brand: {
-          dark: '#1e2d24',     // Forest Green
-          gold: '#c5a880',     // Premium Gold
-          light: '#f7f6f2',    // Off-white/Cream
-          accent: '#2d4b37',   // Active Green
+dark: '#1e2d24',
+          gold: '#c5a880',        // Digunakan untuk dekorasi/aksen
+          'gold-text': '#7d6642', // <--- TAMBAHKAN INI (Emas Gelap untuk Teks)
+          light: '#f7f6f2',
+          accent: '#2d4b37',
         }
       },
       fontFamily: {
